f642c0790e2a33b00a7188a435a0de85b73ac497
jemsAdd <jemlinkdijitech@gmail.com>
2025-05-19 00:39:38 +0300

    to be deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Netflix_2025/'
+  base: '/Netflix_2025/',
+   optimizeDeps: {
+    include: [
+      '@mui/icons-material',
+      '@mui/icons-material/Search', // optional, specific icon
+    ],
+  },
+
 })
